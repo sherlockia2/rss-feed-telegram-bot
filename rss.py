5f4ac817b84771aebdffef52607b6ddf1044414c
sherlockia2 <74177321+sherlockia2@users.noreply.github.com>
2021-07-08 14:31:15 +0530

    Add 'published' to rss feed

--- rss.py
+++ rss.py
@@ -7,6 +7,7 @@ from dotenv import load_dotenv
 from pyrogram import Client, filters
 from pyrogram.errors import FloodWait
 from apscheduler.schedulers.background import BackgroundScheduler
+from dateutil import parser, tz
 
 
 if os.path.exists("config.env"):
@@ -34,14 +35,17 @@ for feed_url in feed_urls:
 
 app = Client(":memory:", api_id=api_id, api_hash=api_hash, bot_token=bot_token)
 
-
+fmt = '%A, %B %d %H:%M:%S'
 def create_feed_checker(feed_url):
     def check_feed():
         FEED = feedparser.parse(feed_url)
         entry = FEED.entries[0]
         if entry.id != db.get_link(feed_url).link:
                        # ↓ Edit this message as your needs.
-            message = f"**{entry.title}**\n```{entry.link}```"
+            dt = parser.parse(entry.published)
+            published = dt.astimezone(tz.gettz('Asia/Kolkata'))
+            date = published.strftime(fmt)
+            message = f"**{entry.title}**\n```{entry.link}```\n**{date}**"
             try:
                 app.send_message(log_channel, message)
                 db.update_link(feed_url, entry.id)
